45edcab6694e0a2cce21850d3aa4567f274feb46
Jinoo Jain <jinoo.jain@gmail.com>
2018-07-11 12:48:22 -0700

    initial app

---
+++ ocr.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const request = require('request');
+var fs = require('fs');
+
+// Replace <Subscription Key> with your valid subscription key.
+const subscriptionKey = 'df57b75cfc1240f9b23236afe29d9337';
+
+// You must use the same location in your REST call as you used to get your
+// subscription keys. For example, if you got your subscription keys from
+// westus, replace "westcentralus" in the URL below with "westus".
+const uriBase =
+    'https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/ocr';
+
+var image_file = fs.readFileSync('./test_images/test_1.jpeg');
+
+const imageUrl = 'https://lchrismanmm.files.wordpress.com/2011/02/pad-1772.jpg'; 
+
+// Request parameters.
+const params = {
+    'language': 'unk',
+    'detectOrientation': 'true',
+};
+
+
+const options = {
+    uri: uriBase,
+    qs: params,
+    body: image_file,
+    headers: {
+        'Content-Type': 'application/octet-stream',
+        'Ocp-Apim-Subscription-Key' : subscriptionKey
+    }
+};
+
+request.post(options, (error, response, body) => {
+  if (error) {
+    console.log('Error: ', error);
+    return;
+  }
+  let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
+  console.log('JSON Response\n');
+  console.log(jsonResponse);
+});
---
+++ v1-ocr.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const request = require('request');
+//var fs = require('fs');
+
+// Replace <Subscription Key> with your valid subscription key.
+const subscriptionKey = 'df57b75cfc1240f9b23236afe29d9337';
+
+// You must use the same location in your REST call as you used to get your
+// subscription keys. For example, if you got your subscription keys from
+// westus, replace "westcentralus" in the URL below with "westus".
+const uriBase =
+    'https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/ocr';
+
+const imageUrl = 'https://static1.squarespace.com/static/56afa81cb654f96eea72642f/t/58408b85ebbd1a12d4fecc65/1495805124872/PD+Car?format=500w'; 
+
+// Request parameters.
+const params = {
+    'language': 'unk',
+    'detectOrientation': 'true',
+};
+
+/*
+fs.readFile('./test_images/test_1.jpeg', (err, data) => {
+  if (err) throw err;
+});
+*/
+
+
+const options = {
+    uri: uriBase,
+    qs: params,
+    body: '{"url": ' + '"' + imageUrl + '"}',
+    headers: {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key' : subscriptionKey
+    }
+};
+
+request.post(options, (error, response, body) => {
+  if (error) {
+    console.log('Error: ', error);
+    return;
+  }
+  let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
+  console.log('JSON Response\n');
+  console.log(jsonResponse);
+});
